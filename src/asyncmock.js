aa3c60b5eba38138bab188e8661014f255ea7e1d
Felipe Villarrubia <felipevillarrubiasymmes@gmail.com>
2022-04-25 12:14:00 -0400

    Agrego counter que desaparece al agregar al carrito y respecta el stock del item

--- src/asyncmock.js
+++ src/asyncmock.js
@@ -58,7 +58,7 @@ const products = [
         category: 'Compact Disc',
         img: '/images/no_va_a_pasar_el_tiempo_en_vano.jpg',
         stock: 20,
-        description: 'Primer Largaduración de Niños del Cerro'
+        description: 'Primer Disco en vivo de Niños del Cerro'
     },
 ]
 
--- src/components/ItemDetail/ItemDetail.js
+++ src/components/ItemDetail/ItemDetail.js
@@ -1,24 +1,84 @@
+import { useState } from 'react'
+import { useNavigate, Link } from 'react-router-dom'
 
-const ItemDetail = ({  name, img, category, description, price, stock }) => {
+const InputCount = ({onConfirm, stock, initial=1}) => {
+    const [count, setCount] = useState(initial)
+    console.log(stock)
+    const handleChange = (e) => {
+        if(e.target.value <= stock) {
+            setCount(e.target.value)
+        }
+    }
+
     return (
-        <div className='card border-0 mt-5 mb-5'>
-            <img src={img} alt={name} className='card-image-top'/>
-            <div class="card-body">
-                <h5 class="card-title">{name}</h5>
-                    <p className="card-text">
-                        Categoria: {category}
-                    </p>
-                    <p className="card-text">
-                        Descripción: {description}
-                    </p>
-                    <p className="card-text">
-                        Precio: {price}
-                    </p>
-                    <p className="card-text">
-                        Stock: {stock}
-                    </p>
-            </div>
+        <div>
+            <input type='number' onChange={handleChange} value={count}/>
+            <button onClick={() => onConfirm(count)}>Agregar al carrito</button>
         </div>
+    )
+}
+
+const ButtonCount = ({ onConfirm, stock, initial = 0 }) => {
+    const [count, setCount] = useState(initial)
+
+    const increment = () => {
+        if (count < stock) {
+            setCount(count +1)
+        }
+}
+
+    const decrement = () => {
+            setCount(count - 1)
+
+    }
+
+    return (
+        <div>
+            <p>{count}</p>
+            <button onClick={decrement}>-</button>
+            <button onClick={increment}>+</button>
+            <button onClick={() => onConfirm(count)}>Agregar al carrito</button>
+        </div>
+    )
+}
+
+const ItemDetail = ({ id, name, img, category, description, price, stock }) => {
+    const [typeInput, setTypeInput] = useState(true)
+    const [quantity, setQuantity] = useState(0) 
+    const navigate = useNavigate()
+
+    const handleAdd = (count) => {
+        console.log('Agregar al carrito')
+        setQuantity(count)
+    }
+
+    const Count = typeInput ? ButtonCount : InputCount
+
+    return (
+        <article className="CardItem">
+            <header className="Header">
+                <h2 className="ItemHeader">
+                    {name}
+                </h2>
+            </header>
+            <picture>
+                <img src={img} alt={name} className="ItemImg"/>
+            </picture>
+            <section>
+                <p className="Info">
+                    Categoria: {category}
+                </p>
+                <p className="Info">
+                    Descripción: {description}
+                </p>
+                <p className="Info">
+                    Precio: {price}
+                </p>
+            </section>           
+            <footer className='ItemFooter'>
+                {quantity > 0 ? <Link to='/cart'>Ir al carrito</Link> : <Count onConfirm={handleAdd} stock={stock}/> } 
+            </footer>
+        </article>
     )
 }
 
